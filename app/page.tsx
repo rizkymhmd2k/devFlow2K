6f5edbd2254edb2debcdb36833022213befc8fb8
Rizky <muhammadrizky1155@gmail.com>
2023-10-05 16:09:26 +0800

    add auth

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Nextjs we are coming</h1>
-      <h1 className="h2-bold">Nextjs we are coming</h1>
-    </div>
-  );
-}
