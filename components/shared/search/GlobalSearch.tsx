cfe54e630d590e9e7337039cc914d3d541f8b1ee
Rizky <muhammadrizky1155@gmail.com>
2023-10-13 15:28:27 +0800

    right sidebar

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,29 +1,26 @@
 import React from "react";
 import Image from "next/image";
-import { Input } from "@/components/ui/input"
-
+import { Input } from "@/components/ui/input";
 
 const GlobalSearch = () => {
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
-      <div 
- className="background-light800_darkgradient relative flex
-      min-h-[56px] grow items-center gap-1 rounded-xl px-4"> 
+    <div className="background-light800_darkgradient flex min-h-[56px] max-w-[600px]
+    flex-1 items-center gap-1 rounded-xl px-4 ">
+
         <Image
-        src="/assets/icons/search.svg"
-        alt="search"
-        width={24}
-        height={24}
-        className="cursor-pointer"
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+          className="cursor-pointer"
         />
         <Input
-        type="text"
-        placeholder="Search Globally"
-        value=""
-        className="paragraph-regular no-focus
+          type="text"
+          placeholder="Search Globally"
+          value=""
+          className="paragraph-regular no-focus
         placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
-      </div>
     </div>
   );
 };
