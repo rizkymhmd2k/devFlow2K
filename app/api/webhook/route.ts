841e28d57b5683274df5652347327c4809f609f9
Rizky <muhammadrizky1155@gmail.com>
2023-10-31 19:26:11 +0800

    add log

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -52,7 +52,8 @@ export async function POST(req: Request) {
  
   const eventType = evt.type;
 
-  console.log({eventType})
+  // console.log({eventType})
+  // add comments
  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
