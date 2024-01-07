931203cc056414cf0de5bb8737ca59071a9b459c
“rizky” <muhammadrizky1155@gmail.com>
2024-01-07 16:02:15 +0800

    community:)

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,7 +2,7 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose"
-import { CreateUserParams, DeleteUserParams, UpdateUserParams } from "./shared.types";
+import { CreateUserParams, DeleteUserParams, GetAllUsersParams, UpdateUserParams } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
 
@@ -77,6 +77,22 @@ export async function deleteUser(params: DeleteUserParams) {
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({})
+      .sort({ createdAt: -1 })
+
+    return { users };
   } catch (error) {
     console.log(error);
     throw error;
