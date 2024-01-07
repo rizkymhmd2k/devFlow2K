6975e3f6e76d2250551630ccf8694f23b9742782
“rizky” <muhammadrizky1155@gmail.com>
2024-01-07 11:50:18 +0800

    fakeID

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -5,9 +5,9 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Page = async () => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
-  // const userId= '123456789'
+  const userId= '123456789'
 
   if (!userId) redirect('/sign-in');
 
