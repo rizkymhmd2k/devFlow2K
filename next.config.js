f6a342e81b4b865e290ff899b646f81592fcfa0b
“rizky” <muhammadrizky1155@gmail.com>
2024-01-07 14:21:59 +0800

    image

--- next.config.js
+++ next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      mdxRs: true,
-      serverComponentsExternalPackages: ['mongoose']
-    },
-    // images: {
-    //   remotePatterns: [
-    //     {
-    //       protocol: 'https',
-    //       hostname: '*'
-    //     }, 
-    //     {
-    //       protocol: 'http',
-    //       hostname: '*'
-    //     }, 
-    //   ]
-    // }
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*'
+      }, 
+      {
+        protocol: 'http',
+        hostname: '*'
+      }, 
+    ]
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
