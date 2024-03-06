67c45b56315c9ba1ee72854e212ef315e9c07310
“rizky” <muhammadrizky1155@gmail.com>
2024-03-06 21:39:57 +0800

    deploy

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
  
-  // Get the headers
+  // Get the headers cek
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
