679039d9413cb8b66414bd82ab47667179db2b32
“rizky” <muhammadrizky1155@gmail.com>
2024-03-09 22:05:42 +0800

    middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhook',
-    'question/:id',
+    '/questions/:id', // Include /questions/:id in the public routes
     '/tags',
     '/tags/:id',
     '/profile/:id',
@@ -12,11 +12,11 @@ export default authMiddleware({
     '/jobs'
   ],
   ignoredRoutes: [
-    '/api/webhook', '/api/chatgpt'
+    '/api/webhook', 
+    '/api/chatgpt'
   ]
 });
  
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
